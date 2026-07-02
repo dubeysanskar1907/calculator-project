26acc3e8bc2d002c2a75821c3fd92916ba9d8031
Sanskardubey <sanskardubey@gmail.com>
2026-07-02 15:33:49 +0530

    Add multiplication feature

--- calculate.py
+++ calculate.py
@@ -2,7 +2,9 @@ def calculate():
     print("--- Calculator ---")
     a = float(input("Enter first number: "))
     b = float(input("Enter second number: "))
+    
     print(f"Addition: {a + b}")
+    print(f"Multiplication: {a * b}")
 
 if __name__ == "__main__":
     calculate()
